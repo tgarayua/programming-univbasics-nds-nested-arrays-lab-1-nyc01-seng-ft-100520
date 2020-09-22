4c57e8b241c86c41754fa5eb05ae958c5397b54a
Thomas Garayua <tgarayua@gmail.com>
2020-09-22 23:07:12 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,4 +1,5 @@
 # Examples inspired by U. S. National Organic Standards
+require 'pry'
 
 ORGANIC_PRODUCE = [
   "Strawberries",
@@ -45,5 +46,6 @@ def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
-  return matrix[row][column]
+  binding.pry
+  return matrix[row][column][new_value]
 end
